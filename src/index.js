02077013e864ef07517ad9984308ca3360efd918
mi1sh <106558234+mi1sh@users.noreply.github.com>
2023-01-29 19:54:13 +0600

    add react

--- src/index.js
+++ src/index.js
@@ -1,50 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
 
-const initialState = {value: 0};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'INC': 
-      return {...state, value: state.value + 1};
-    case 'DEC':
-      return {...state, value: state.value - 1};
-    case 'RND':
-      return {...state, value: state.value * action.payload};
-    default:
-      return state;
-  }
-}
+import App from './components/App';
 
 const store = createStore(reducer);
 
-const update = () => {
-  document.getElementById('counter').textContent = store.getState().value;
-}
-
-store.subscribe(update);
-
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = (value) => ({type: 'RND', payload: value});
-
-document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc());
-});
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec());
-});
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10)
-  store.dispatch(rnd(value));
-});
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <>
-
-    </>
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </React.StrictMode>
+  );
